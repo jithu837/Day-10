a28df9b7807d51cd0ef57b4c36664f7590b777ab
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-11 12:36:58 +0530

    Add files via upload

---
+++ PrivateAccessStatic.java
@@ -0,0 +1,13 @@
+class PrivateAccessStatic{
+    private static void s()
+    {
+        System.out.println("Hello");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+
+
+    
+}
